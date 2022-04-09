512ced2c28cf518a6c73ae411fa8c627583baf61
luizfm <luiz.ado@hotmail.com>
2022-04-09 17:32:44 -0300

    feat: Add projects-page

--- src/components/accordion/index.jsx
+++ src/components/accordion/index.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
+import classnames from 'classnames';
 import { prismicSlicePropTypes } from '_utils/proptypes';
 
-const Accordion = ({ slice }) => {
-  console.log({ slice });
-  return (
-    <section>
-      <h1>Os projetos disponibilizados pela CVM</h1>
-      <div>
-        {slice.items.map((item) => (
-          <div>
-            <h2>{item.title}</h2>
-            <p>{item.text}</p>
-          </div>
-        ))}
-      </div>
-    </section>
-  );
-};
+import ArrowDownIcon from '_assets/icons/arrow-down-icon.svg';
+import IconButton from '_components/icon-button';
+
+import styles from './styles.css';
+import AccordionItem from './accordion-item';
+
+const Accordion = ({ slice }) => (
+  <section className={styles['accordion-container']}>
+    <h1 className={styles['accordion-section-title']}>
+      Os projetos disponibilizados pela CVM
+    </h1>
+    <div className={styles['accordion-wrapper']}>
+      {slice.items.map((item) => (
+        <AccordionItem key={item.title} accordionItem={item} />
+      ))}
+    </div>
+  </section>
+);
 
 Accordion.propTypes = {
   slice: prismicSlicePropTypes.isRequired,
