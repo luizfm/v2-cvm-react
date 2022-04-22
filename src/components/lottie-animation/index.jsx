56289f3966794a28fe5850bfde1a93c3832644ac
luizfm <luiz.ado@hotmail.com>
2022-04-22 09:48:11 -0300

    fix: Fix responsiveness on home-page

--- src/components/lottie-animation/index.jsx
+++ src/components/lottie-animation/index.jsx
@@ -2,10 +2,13 @@ import React, { useMemo } from 'react';
 import Lottie from 'react-lottie';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
+import useWindowSize from '_hooks/use-window-size';
 
 import styles from './styles.css';
 
 const LottieAnimation = ({ lottieFile, height, width, className }) => {
+  const { isMobile } = useWindowSize();
+
   const defaultOptions = useMemo(
     () => ({
       loop: true,
@@ -18,9 +21,26 @@ const LottieAnimation = ({ lottieFile, height, width, className }) => {
     [lottieFile]
   );
 
+  const { lottieHeight, lottieWidth } = useMemo(() => {
+    if (height && width) {
+      return {
+        lottieHeight: height,
+        lottieWidth: width,
+      };
+    }
+
+    return isMobile
+      ? { lottieHeight: 280, lottieWidth: 280 }
+      : { lottieHeight: 400, lottieWidth: 400 };
+  }, [height, isMobile, width]);
+
   return (
     <div className={classnames(styles['lottie-container'], className)}>
-      <Lottie options={defaultOptions} height={height} width={width} />
+      <Lottie
+        options={defaultOptions}
+        height={lottieHeight}
+        width={lottieWidth}
+      />
     </div>
   );
 };
@@ -33,8 +53,8 @@ LottieAnimation.propTypes = {
 };
 
 LottieAnimation.defaultProps = {
-  height: 400,
-  width: 400,
+  height: null,
+  width: null,
   className: '',
 };
 
